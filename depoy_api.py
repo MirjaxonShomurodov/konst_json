ac50d04f68ae992eae3cc5f1c76e75efb6ff2a0b
Mirjaxon <mirjaxon457@gmail.com>
2024-03-26 14:32:15 +0500

    telegram

--- depoy_api.py
+++ depoy_api.py
@@ -1,12 +1,15 @@
 from flask import Flask,request
+
 import telegram
+
 TOKEN = "5923509687:AAE04EHlrwqvh7cGCIyXrOn3obe_cAwPuys"
 bot = telegram.Bot(TOKEN)
+
 app = Flask(__name__)
 
 @app.route('/',methods=['POST'])
 def main():
-    data = request.git_json()
+    data = request.get_json()
     chat_id = data['message']['id']
     bot.send_message(chat_id=chat_id,text=data['message']['text'])
     return "hello world"
